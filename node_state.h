a094621f1fc2975e705971dc1c0fb1cae232c73c
Mieloch <pawel.mieloch@gmail.com>
2017-06-08 17:50:36 +0200

    comment

--- node_state.h
+++ node_state.h
@@ -1,6 +1,7 @@
 #ifndef NODE_STATE_STRUCTURE_H
 #define NODE_STATE_STRUCTURE_H
 #include "node_structure.h"
+// strutkura pelniaca role pamieci globalnej pomiedzy watkami, tutaj chyba kolejki, wskaznik na wlasciciela tokena, itd
 typedef struct{
     node* node_data;
     int resource_owner;
