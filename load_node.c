4b71fabcff9b4fbf97726039a5f8e6155d4d815e
Mieloch <pawel.mieloch@gmail.com>
2017-06-08 12:29:35 +0200

    node load

---
+++ load_node.c
@@ -0,0 +1,154 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "node_structure.h"
+
+void get_field(char* line, char* buff, int num){
+    int len = strlen(line);
+    int l_s = num;
+    int lowe_sep_index = 0;
+    {
+        int i=0;
+        int count = 0;
+        for(i=0;i< len; i++){
+            if(line[i] == ';'){
+                count++;
+            }
+            if(count == l_s){
+                lowe_sep_index = i+1;
+                break;
+            }
+        }
+    }
+    int col_len = 0;
+    int j =lowe_sep_index;
+    while(line[j] != ';'){
+        col_len++;
+        j++;
+    }
+    memcpy(buff,&line[lowe_sep_index], col_len * sizeof(char));
+    buff[col_len] = '\0';
+}
+
+int* column_to_int_arr(char* column){
+    if(column[0] == 'x'){
+        return NULL;
+    }
+    int i = 0;
+    int sep_count = 0;
+    while(column[i] != '\0'){
+        if(column[i] == ','){
+            sep_count++;
+        }
+        i++;
+    }
+    int len = sep_count + 1;
+    int* arr = malloc(len * sizeof(int));
+
+    int j =0;
+    char* token = strtok(column, ",");
+    while(token){
+        arr[j] = atoi(token);
+        j++;
+        token = strtok(0,",");
+    }
+    return arr;
+
+}
+
+int find_id(char* line){
+        char buff[3];
+        get_field(line,buff,1);
+        int id;
+        if(column_to_int_arr(buff) == NULL){
+            id = -1;
+        }
+        else{
+            id = column_to_int_arr(buff)[0];
+        }
+        return id;
+}
+
+int* find_siblings(char* line){
+    char buff[20];
+    get_field(line,buff, 3);
+    return column_to_int_arr(buff);
+}
+
+int find_parent(char* line){
+    char buff[20];
+    get_field(line, buff, 2);
+    int parent;
+    if(column_to_int_arr(buff) == NULL){
+        parent = -1;
+    }
+    else{
+        parent = column_to_int_arr(buff)[0];
+    }
+    return parent;
+}
+
+int* find_children(char* line){
+    char buff[20];
+    get_field(line, buff, 4);
+    return column_to_int_arr(buff);
+}
+int arr_len(int* arr){
+    return sizeof(arr)/sizeof(arr[0]) +1;
+}
+void print_arr(int * arr, char* arr_name){
+    printf("%s = ", arr_name);
+    if(arr == NULL){
+        printf("none ");
+        return;
+    }
+    int len = arr_len(arr);
+    int i = 0;
+    for(i=0;i<len;i++){
+        printf("%d, ", arr[i]);
+    }
+}
+
+node* load_node()
+{
+    int pid = 4; //mock
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    node* my_node = malloc(sizeof(node));
+    fp = fopen("node_hierarchy", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        int id = find_id(line);
+        if(pid == id){
+            int* children = find_children(line);
+            int parent = find_parent(line);
+            int * siblings = find_siblings(line);
+            my_node->id = id;
+            my_node->parent = parent;
+            my_node->children = children;
+            my_node->siblings = siblings;
+            my_node->children_length = arr_len(children);
+            my_node->siblings_length = arr_len(siblings);
+        }
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    return my_node;
+}
+int main(void){
+    node* my_node = load_node();
+    print_arr(my_node->siblings, "siblings");
+    print_arr(my_node->children, "children");
+    printf("parent = %d ", my_node->parent);
+    printf("id = %d", my_node->id);
+    printf("\n\n");
+    return 0;
+
+}
--- node_structure.h
+++ node_structure.h
@@ -1,12 +1,13 @@
 #ifndef NODE_STRUCTURE
 #define NODE_STRUCTURE
 
-struct node {
+typedef struct{
+    int id;
     int parent;
     int * children;
     int * siblings;
     int children_length;
     int siblings_length;
-};
+}node;
 
 #endif
